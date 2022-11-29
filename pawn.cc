7d4e49472c14e8935c850d2c05f67f8fc6ab5d3f
a589shar <a589shar@uwaterloo.ca>
2022-11-28 22:42:06 -0500

    some changes

--- pawn.cc
+++ pawn.cc
@@ -23,83 +23,8 @@ bool Pawn::isInPath(char oldPiece, char oldCol, int oldRow,
     for (int i = oldRow - 1; i >= newRow; --i) {
       if(isOccupied(i, oldCol)) return false;
     }
-    return true;
-  } else if (oldPiece == 'r') {
-    // two things for rook
-    // it is bidirection so need to consider if its going left or right or 
-    // up or down, then you need to check if from the spot it is now
-    // to the spot it wants to go to, is there anything blocking its way.
-    // if there is then dont let it move. if there isnt then let it move.
-    if (oldCol == newCol) { // up and down
-      if (oldRow <= newRow) {
-        for (int i = oldRow + 1; i <= newRow; ++i) {
-          if (isOccupied(i, oldCol)) return false;
-        }
-      } else {
-        for (int i = oldRow - 1; i >= newRow; --i) {
-          if (isOccupied(i, oldCol)) return false;
-        }
-      }
-    } else { // left and right
-      if (oldCol <= newCol) {
-        for(int i = oldCol + 1; i <= newCol; ++i) {
-          if (isOccupied(oldRow, i)) return false;
-        }
-      } else {
-        for(int i = oldCol - 1; i >= newCol; --i) {
-          if (isOccupied(oldRow, i)) return false;
-        }
-      }
-    }
-    return true;
-  } else if (oldPiece == 'R') {
-    if (oldCol == newCol) { // up and down
-      if (oldRow <= newRow) {
-        for (int i = oldRow + 1; i <= newRow; ++i) {
-          if (isOccupied(i, oldCol)) return false;
-        }
-      } else {
-        for (int i = oldRow - 1; i >= newRow; --i) {
-          if (isOccupied(i, oldCol)) return false;
-        }
-      }
-    } else { // left and right
-      if (oldCol <= newCol) {
-        for(int i = oldCol + 1; i <= newCol; ++i) {
-          if (isOccupied(oldRow, i)) return false;
-        }
-      } else {
-        for(int i = oldCol - 1; i >= newCol; --i) {
-          if (isOccupied(oldRow, i)) return false;
-        }
-      }
-    }
-    return true;
-  } else if (oldPiece == 'n') {
-
-    return true;
-  } else if (oldPiece == 'N') {
-
-    return true;
-  } else if (oldPiece == 'b') {
-
-    return true;
-  } else if (oldPiece == 'B') {
-
-    return true;
-  } else if (oldPiece == 'q') {
-
-    return true;
-  } else if (oldPiece == 'Q') {
-
-    return true;
-  } else if (oldPiece == 'k') {
-
-    return true;
-  } else {
-    
-    return true;
   }
+  return true;
 }
 
 bool Pawn::isValidMove(char oldPiece, char oldCol, int oldRow, 
