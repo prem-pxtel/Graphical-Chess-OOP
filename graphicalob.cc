42b7d49228f955a7677d7a6189b48e4ad1a3bc64
p352pate <p352pate@uwaterloo.ca>
2022-12-04 03:05:19 -0500

    modified x and y offset in graphical observer class

--- graphicalob.cc
+++ graphicalob.cc
@@ -4,22 +4,28 @@
 
 GraphicalOb::GraphicalOb(Board *b) 
   : b{b} {
-  window = new Xwindow{50 * 8, 50 * 8};
+  int cellWidth = 50;
+  window = new Xwindow{cellWidth * 8, cellWidth * 8};
   b->attach(this);
 }
 
 void GraphicalOb::notify() {
+  int cellWidth = 50;
+  int xOffset = 22;
+  int yOffset = 29;
   for (int row = 0; row < 8; ++row) {
     for (int col = 0; col < 8; ++col) {
       char cur = b->getPiece(row + 1, col + 'a');
       std::string pieceChar;
       pieceChar += cur;
       int colour;
-      if ((row + col) % 2 == 1) colour = Xwindow::White;
+      if ((row + col) % 2 == 0) colour = Xwindow::White;
       else colour = Xwindow::Green;
-      window->fillRectangle(50 * col, 50 * row, 50, 50, colour);
+      window->fillRectangle(cellWidth * col, cellWidth * row, 
+                            cellWidth, cellWidth, colour);
       if (cur != ' ' && cur != '_') {
-        window->drawString(50 * col + 23, 50 * row + 28, pieceChar);
+        window->drawString(cellWidth * col + xOffset, 
+                           cellWidth * row + yOffset, pieceChar);
       }
     }
   }
